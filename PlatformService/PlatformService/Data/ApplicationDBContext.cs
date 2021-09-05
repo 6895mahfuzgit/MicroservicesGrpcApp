8adf0d6765295488439702f150ce9195f1165d6c
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-06 00:44:57 +0600

    Added Application Context  and Register in Startup

---
+++ PlatformService/PlatformService/Data/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+    }
+
+
+}
