93b647946a8cdafb33328aaffbc7a6bb63ab9824
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-07 01:19:38 +0600

    Create Get all Platform Api

---
+++ PlatformService/PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _platformRepo;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo platformRepo, IMapper mapper)
+        {
+            _platformRepo = platformRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatfroms()
+        {
+            try
+            {
+                var platfromItems = _platformRepo.GetAllPlatfrom();
+                return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platfromItems));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
