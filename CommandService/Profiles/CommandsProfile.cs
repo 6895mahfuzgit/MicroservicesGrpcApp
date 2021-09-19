2fabb8eee7405dc1f3362507103671d7f5956a4c
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-20 00:15:56 +0600

    Create CommandsProfile.cs

---
+++ CommandService/Profiles/CommandsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CommandService.Dtos;
+using CommandService.Models;
+
+namespace CommandService.Profiles
+{
+    public class CommandsProfile : Profile
+    {
+        public CommandsProfile()
+        {
+            //map work as source to target 
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<CommandReadDto, Command>();
+            CreateMap<Command, CommandReadDto>();
+        }
+    }
+}
