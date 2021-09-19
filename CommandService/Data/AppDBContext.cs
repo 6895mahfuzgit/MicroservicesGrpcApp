7a7fdd85c0a3feb67bb563daae64e5ef66d2dbe8
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-19 23:37:39 +0600

    Added Application Context in CommandService Project

---
+++ CommandService/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using CommandService.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandService.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Command> Commands { get; set; }
+    }
+
