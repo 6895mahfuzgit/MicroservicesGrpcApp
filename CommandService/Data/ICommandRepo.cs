8c5ddba60e4137b9681c484312bf6c223c1d2e4c
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-19 23:48:23 +0600

    Create ICommandRepo.cs

---
+++ CommandService/Data/ICommandRepo.cs
@@ -0,0 +1,24 @@
+﻿using CommandService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+
+        //PlatFroms
+        IEnumerable<Platform> GetPlatforms();
+        void CreatePlatfrom(Platform platform);
+        bool PlatfromExists(int platfromId);
+
+        //Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platfromId);
+        Command GetCommand(int platfromId, int commandId);
+        void CreateCommand(int platfromId, Command command);
+
+    }
+}
