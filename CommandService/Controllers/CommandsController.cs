40506ce18da20b0c9d287fe21c2ab6cb33feafa8
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-21 00:37:43 +0600

    Create Command Controller and Create GetCommandForPlatfrom To Get All Platform Commands

---
+++ CommandService/Controllers/CommandsController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CommandService.Data;
+using CommandService.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandService.Controllers
+{
+    [Route("api/c/platforms/{platformId}/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommandRepo _commandRepo;
+        private readonly IMapper _mapper;
+
+        public CommandsController(ICommandRepo commandRepo, IMapper mapper)
+        {
+            _commandRepo = commandRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId)
+        {
+            try
+            {
+                Console.WriteLine(" Called GetCommandsForPlatform Action");
+                if (!_commandRepo.PlatfromExists(platformId))
+                {
+                    return NotFound();
+                }
+
+                var commands = _commandRepo.GetCommandsForPlatform(platformId);
+                return Ok(_mapper.Map<IEnumerable<CommandReadDto>>(commands));
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+
+
+
+    }
+}
