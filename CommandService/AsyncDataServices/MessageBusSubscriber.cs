187fea5ca394350921b470aca4c80839a5a717bc
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-25 01:05:30 +0600

    Update MessageBusSubscriber.cs

--- CommandService/AsyncDataServices/MessageBusSubscriber.cs
+++ CommandService/AsyncDataServices/MessageBusSubscriber.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using CommandService.EventProcessing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,6 +9,17 @@ namespace CommandService.AsyncDataServices
 {
     public class MessageBusSubscriber : BackgroundService
     {
+        private readonly IConfiguration _configuration;
+        private readonly IEventProcessor _eventProcessor;
+
+        public MessageBusSubscriber(IConfiguration configuration, IEventProcessor eventProcessor)
+        {
+            _configuration = configuration;
+            _eventProcessor = eventProcessor;
+        }
+
+
+
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
         {
             throw new NotImplementedException();
