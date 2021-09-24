6b3486a73da6b6c24619c312b308f5c69b3791f0
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-24 20:26:28 +0600

    Create MessageBusClient.cs

---
+++ PlatformService/PlatformService/AsyncDataServices/MessageBusClient.cs
@@ -0,0 +1,16 @@
+﻿using PlatformService.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformService.AsyncDataServices
+{
+    public class MessageBusClient : IMessageBusClient
+    {
+        public void PublishNewPlatform(PlatfromPublishedDto platfromPublishedDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
