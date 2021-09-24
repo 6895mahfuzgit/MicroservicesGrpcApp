6291ac7298874bb89a81c412d5be9764c0ef6a31
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-24 22:48:07 +0600

    Create PlatfromPublishedDto.cs

---
+++ CommandService/Dtos/PlatfromPublishedDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandService.Dtos
+{
+    public class PlatfromPublishedDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Event { get; set; }
+    }
+}
