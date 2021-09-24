670e68bbc5c52ee8bbc9c6100c22d97fbb85e13f
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-24 23:39:48 +0600

    Create MessageBusSubscriber.cs

---
+++ CommandService/AsyncDataServices/MessageBusSubscriber.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommandService.AsyncDataServices
+{
+    public class MessageBusSubscriber : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
