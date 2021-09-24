eeb4504e87aded60a991dfdf040aa4b31c3466a9
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-24 23:02:46 +0600

    Create IEventProcessor.cs

---
+++ CommandService/EventProcessing/IEventProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandService.EventProcessing
+{
+    public interface IEventProcessor
+    {
+        void Process(string message);
+    }
+}
