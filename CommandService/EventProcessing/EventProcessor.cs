740a342dcc1ee06adddc4bac0f38ca71d7ab87b4
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-24 23:21:18 +0600

    Update EventProcessor.cs

--- CommandService/EventProcessing/EventProcessor.cs
+++ CommandService/EventProcessing/EventProcessor.cs
@@ -1,17 +1,46 @@
-﻿using System;
+﻿using AutoMapper;
+using CommandService.Dtos;
+using CommandService.Enums;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CommandService.EventProcessing
 {
     public class EventProcessor : IEventProcessor
     {
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+        private IMapper _mapper;
+
+        public EventProcessor(IServiceScopeFactory serviceScopeFactory, IMapper mapper)
+        {
+            _serviceScopeFactory = serviceScopeFactory;
+            _mapper = mapper;
+        }
         public void Process(string message)
         {
             throw new NotImplementedException();
         }
+
+        private EventType DetermineEvent(string notificatiobMsg)
+        {
+            Console.WriteLine(" DetermineEvent Start");
+
+            var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificatiobMsg);
+            switch (eventType.Event)
+            {
+                case "Platform_Published":
+                    Console.WriteLine("Platform_Published Detcted in Enum Switch");
+                    return EventType.PlatformPublished;
+                default:
+                    Console.WriteLine("Can't Determine Event Type.");
+                    return EventType.Undetermined;
+            }
+        }
     }
 
-    
+
 }
