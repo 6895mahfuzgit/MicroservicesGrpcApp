ea245da01ebc6fdc266f6efa41fb4625c950dfd2
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-24 23:07:39 +0600

    Create EventProcessor.cs

---
+++ CommandService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandService.EventProcessing
+{
+    public class EventProcessor : IEventProcessor
+    {
+        public void Process(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+}
