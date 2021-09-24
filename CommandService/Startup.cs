4ed160a9b7df4a72e9c311dbac326e389ce4d430
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-25 01:27:00 +0600

    Update Startup.cs

--- CommandService/Startup.cs
+++ CommandService/Startup.cs
@@ -1,3 +1,4 @@
+using CommandService.AsyncDataServices;
 using CommandService.Data;
 using CommandService.EventProcessing;
 using Microsoft.AspNetCore.Builder;
@@ -35,6 +36,7 @@ namespace CommandService
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddControllers();
+            services.AddHostedService<MessageBusSubscriber>();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
