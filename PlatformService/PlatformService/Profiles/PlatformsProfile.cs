02213ff69c2b591f8daf2c515187c5a03069a9ff
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-25 20:08:09 +0600

    Update PlatformsProfile.cs

--- PlatformService/PlatformService/Profiles/PlatformsProfile.cs
+++ PlatformService/PlatformService/Profiles/PlatformsProfile.cs
@@ -12,6 +12,11 @@ namespace PlatformService.Profiles
             CreateMap<Platform, PlatformReadDto>();
             CreateMap<PlatfromCreateDto, Platform>();
             CreateMap<PlatformReadDto, PlatfromPublishedDto>();
+            CreateMap<Platform, GrpcPlatformModel>()
+                .ForMember(x => x.PlatformId, obj => obj.MapFrom(src => src.Id))
+                .ForMember(x => x.Name, obj => obj.MapFrom(src => src.Name))
+                .ForMember(x => x.Publisher, obj => obj.MapFrom(src => src.Publisher))
+                ;
         }
     }
 }
