670e880685e732a20807f8be5e6844b7c8cb3ffe
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-25 23:26:47 +0600

    Create PrepDb.cs

---
+++ CommandService/Data/PrepDb.cs
@@ -0,0 +1,38 @@
+﻿using CommandService.Models;
+using CommandService.SyncDataServices.Grpc;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+
+namespace CommandService.Data
+{
+    public static class PrepDb
+    {
+        public static void PrepPopulation(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var grpcClient = serviceScope.ServiceProvider.GetService<IPlatformDataClient>();
+
+                var platforms = grpcClient.ReturnAllPlatforms();
+
+                SeedData(serviceScope.ServiceProvider.GetService<ICommandRepo>(), platforms);
+            }
+        }
+
+        private static void SeedData(ICommandRepo repo, IEnumerable<Platform> platforms)
+        {
+            Console.WriteLine("Seeding new platforms...");
+
+            foreach (var plat in platforms)
+            {
+                if (!repo.ExternalPlatfromExists(plat.ExternalID))
+                {
+                    repo.CreatePlatfrom(plat);
+                }
+                repo.SaveChanges();
+            }
+        }
+    }
+}
