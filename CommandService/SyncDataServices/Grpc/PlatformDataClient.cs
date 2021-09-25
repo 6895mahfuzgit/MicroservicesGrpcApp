398034dbc7d1c04b508ee07f7270e591199658fd
Md Mahfuzur Rahman <6895mahfuz@gmail.com>
2021-09-25 23:26:39 +0600

    Create PlatformDataClient.cs

---
+++ CommandService/SyncDataServices/Grpc/PlatformDataClient.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CommandService.Models;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+using PlatformService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandService.SyncDataServices.Grpc
+{
+    public class PlatformDataClient : IPlatformDataClient
+    {
+
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
+
+        public PlatformDataClient(IConfiguration configuration, IMapper mapper)
+        {
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<Platform> ReturnAllPlatforms()
+        {
+            Console.WriteLine($"Called GRPC Service {_configuration["GrpcPlatform"]}");
+            var channel = GrpcChannel.ForAddress(_configuration["GrpcPlatform"]);
+            var client = new GrpcPlatform.GrpcPlatformClient(channel);
+            var request = new GetAllRequest();
+
+            try
+            {
+                var reply = client.GetAllPlatforms(request);
+                Console.WriteLine("Client Worked!!!!!!!!!");
+                return _mapper.Map<IEnumerable<Platform>>(reply);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Can't Call GRPC Server. Error: {ex.Message}");
+                return Enumerable.Empty<Platform>();
+            }
+        }
+    }
+}
